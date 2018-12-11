f6f198419ff6c3439377e01a94602354295867bf
MarsaMD <mmdaffa@gmail.com>
2018-12-12 01:28:49 +0700

    pesanan saya nih

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Online Tender</title>
+    <title>Weply!</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -26,7 +26,7 @@
 
     <h1 class="site-heading text-center text-white d-none d-lg-block">
       <span class="site-heading-upper text-primary mb-3">A Good Food And Clothing For All Your Daily Needs</span>
-      <span class="site-heading-lower">Online Tender</span>
+      <span class="site-heading-lower">Weply!</span>
     </h1>
 
     <!-- Navigation -->
@@ -39,18 +39,18 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav mx-auto">
             <li class="nav-item px-lg-4">
-              <a class="nav-link text-uppercase text-expanded" href="index">Home
+              <a class="nav-link text-uppercase text-expanded" href="index">Beranda
                 <span class="sr-only">(current)</span>
               </a>
             </li>
             <li class="nav-item px-lg-4">
-              <a class="nav-link text-uppercase text-expanded" href="about">About</a>
+              <a class="nav-link text-uppercase text-expanded" href="about">Toko</a>
             </li>
             <li class="nav-item active px-lg-4">
-              <a class="nav-link text-uppercase text-expanded" href="products">Products</a>
+              <a class="nav-link text-uppercase text-expanded" href="products">Pesanan Saya</a>
             </li>
             <li class="nav-item px-lg-4">
-            <a class="nav-link text-uppercase text-expanded" href="login">Login</a>
+            <a class="nav-link text-uppercase text-expanded" href="login">Masuk</a>
             </li>
           </ul>
         </div>
@@ -58,23 +58,82 @@
     </nav>
 
 <section class="page-section clearfix">
-      <div class="container">
-        <div class="intro">
-          <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="img/rompi.jpg" alt="">
-          <div class="intro-text left-0 text-center bg-faded p-5 rounded">
-            <h2 class="section-heading mb-4">
-              <span class="section-heading-lower">Rompi</span>
-            </h2>
-            <div class="intro-button mx-auto">
-            </div>
+      <form method="POST" action="konfirmasi">
+          @csrf
+
+          <input type="hidden" id="order_type" name="order_type" value="wedding">
+
+          <div class="form-group row">
+              <label for="Nama" class="col-sm-4 col-form-label text-md-right">{{ __('Nama') }}</label>
+
+              <div class="col-md-6">
+                  <input id="Nama" type="text" class="form-control{{ $errors->has('Nama') ? ' is-invalid' : '' }}" name="Nama" required autofocus>
+
+                  @if ($errors->has('Jenis Pakaian/Makanan'))
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $errors->first('Nama') }}</strong>
+                      </span>
+                  @endif
+              </div>
           </div>
-        </div>
-      </div>
+
+          <div class="form-group row">
+              <label for="Nomor Telepon" class="col-sm-4 col-form-label text-md-right">{{ __('Nomor Telepon') }}</label>
+
+              <div class="col-md-6">
+                  <input id="Nomor Telepon" type="text" class="form-control{{ $errors->has('Nomor Telepon') ? ' is-invalid' : '' }}" name="Nomor Telepon" required autofocus>
+
+                  @if ($errors->has('Jenis Pakaian/Makanan'))
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $errors->first('Nomor Telepon') }}</strong>
+                      </span>
+                  @endif
+              </div>
+          </div>
+
+          <div class="form-group row">
+              <label for="Jenis Pakaian/Makanan" class="col-sm-4 col-form-label text-md-right">{{ __('Jenis Pakaian/Makanan') }}</label>
+
+              <div class="col-md-6">
+                  <input id="Jenis Pakaian/Makanan" type="text" class="form-control{{ $errors->has('Jenis Pakaian/Makanan') ? ' is-invalid' : '' }}" name="Jenis Pakaian/Makanan" required autofocus>
+
+                  @if ($errors->has('Jenis Pakaian/Makanan'))
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $errors->first('Jenis Pakaian/Makanan') }}</strong>
+                      </span>
+                  @endif
+              </div>
+          </div>
+           <div class="form-group row">
+              <label for="Jumlah Pesanan" class="col-sm-4 col-form-label text-md-right">{{ __('Jumlah Pesanan') }}</label>
+
+              <div class="col-md-6">
+                  <input id="Jumlah Pesanan" type="text" class="form-control{{ $errors->has('Jumlah Pesanan') ? ' is-invalid' : '' }}" name="Jumlah Pesanan" required autofocus>
+
+                  @if ($errors->has('Jumlah Pesanan'))
+                      <span class="invalid-feedback" role="alert">
+                          <strong>{{ $errors->first('Jumlah Pesanan') }}</strong>
+                      </span>
+                  @endif
+              </div>
+          </div>
+
+
+
+          <div class="form-group row mb-0">
+              <div class="col-md-8 offset-md-4">
+                  <button type="submit" class="btn btn-primary">
+                      {{ __('Pesan') }}
+                  </button>
+              </div>
+          </div>
+      </form>
     </section>
+
 
     <footer class="footer text-faded text-center py-5">
       <div class="container">
-        <p class="m-0 small">Copyright &copy; Teol.co.id 2018</p>
+        <p class="m-0 small">Copyright &copy; Weply!.co.id 2018</p>
       </div>
     </footer>
 
